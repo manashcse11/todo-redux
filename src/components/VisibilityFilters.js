d305fc764376bd1cf239523a58bd39ccea6abe50
Manash <manash@cefalo.com>
2020-08-07 13:08:26 +0600

    Complete VisibilityFilter component

--- src/components/VisibilityFilters.js
+++ src/components/VisibilityFilters.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import cx from 'classnames';
 
-const VisibilityFilters = () => {
-    return ('');
+const VisibilityFilters = ({ activeFilter }) => {
+    const filters = {
+        "all": "All"
+        , "completed": "Completed"
+        , "incomplete": "Incomplete"
+    };
+
+    return (
+        <div>
+            {Object.keys(filters).map((key) => {
+                return <span key={`visibility-filter-${key}`} className={cx("filter", key === activeFilter && "filter-active")} onClick={() => {}}>
+                    {filters[key]}
+                </span>;
+            })}
+        </div>
+    );
 }
 
 export default VisibilityFilters;
